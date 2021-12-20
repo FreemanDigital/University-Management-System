10f140b246b8bd642d54f1199bcba0a923f92536
daedren30 <76187727+daedren30@users.noreply.github.com>
2021-12-19 22:16:27 -0700

    Resource protect and dropdown, button styling
    - Resources can only be accessed by logged in user
    - One dropdown for resources
    - Style buttons for sign up/in, log out, and edit profile
    - Removed turbolinks (interfering with dropdown)

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,4 +1,5 @@
 class SectionsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_section, only: %i[ show edit update destroy ]
 
   # GET /sections or /sections.json
